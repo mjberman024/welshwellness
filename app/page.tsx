aa11b7af107059a579af2645f61e346d6f40cde4
Matthew Berman <mjberman@matthews-mbp-3.mynetworksettings.com>
2024-08-13 09:36:36 -0400

    Images deploy

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,18 @@
-import Image from "next/image";
 import styles from "./page.module.css";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <main className={styles.main}>
+      <div style={{ width: "300px", height: "325px", position: "relative" }}>
+        <Image
+          className="headshot_img"
+          src="https://welsh-wellness.s3.amazonaws.com/images/headshot.JPG"
+          alt="Erin headshot"
+          fill
+          objectFit="contain"
+        />
+      </div>
       <p>Welsh Wellness and Counseling!</p>
     </main>
   );
--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,16 @@
 const isProd = process.env.NODE_ENV;
 
 const nextConfig = {
-  basePath: isProd ? "" : "",
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "welsh-wellness.s3.amazonaws.com",
+        port: "",
+        pathname: "/images/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
