374800d328a25864dfe554f33cd4cbc2cea05cf5
Matthew Berman <mjberman@matthews-mbp-3.mynetworksettings.com>
2024-08-13 09:12:57 -0400

    Deploy check

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const isProd = process.env.NODE_ENV;
+
+const nextConfig = {
+  basePath: isProd ? "" : "",
+};
 
 export default nextConfig;
